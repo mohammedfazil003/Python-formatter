b674c11d81a2aa3316f2dc76027d1960c4001341
Mohammed Fazil <mohammed.fazil@softwareag.com>
2022-02-04 17:46:46 +0530

    formatter check files to be committed

---
+++ src/sample1.py
@@ -0,0 +1,19 @@
+"""
+ Copyright (c) 2018-2022 Germany, INDIA, Australia, Canada, England, software Africa, Turkey,
+  West-Indies, Korea,Netherland.
+ """
+
+import os  # noqa
+
+
+def print_morning(name):
+    print(f"Hi , {name}")
+
+
+print_morning("Welcome")
+
+abc = {1, 1, 1, 1, 2, 2, 2, 2, 2}
+
+xyz = [1, 2, 3, 4, 5, 6]
+
+scale = "We had a decision about the black and flake8 we need to look for length as both the library length varies."
