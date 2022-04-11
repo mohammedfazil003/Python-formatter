9f61476f5b62d8a185acf95e93bf3b12929451c8
Mohammed Fazil <mohammed.fazil@softwareag.com>
2022-04-11 18:06:48 +0530

    updated project for verification

--- src/sample1.py
+++ src/sample1.py
@@ -3,23 +3,23 @@
   West-Indies, Korea,Netherland.
  """
 
-import os, random
-
-
+import os, random 
 
 # Method with one argument
-
-
 def print_moning(name="Black"):
     value = random.random()
     print(f"Hi,{name}")
 
-
 print_morning("formatter")
 
-abc = {1,1,1,1,2, 2, 2, 2, 2}
 
-scale = "We had a decision about the h varies. We had a decision about the black and flake8 we need to look for length as both the library length varies."
+abc = {1,1,1, 1, 2, 2, 2, 2, 2}
+
+
+
+cd = {"name", "zementis", "age", 23, "no", 89101010011110, "after", "pycharm", 12345567778, 123, 2223212333}
+
+scale = "We had a decision about the h varies. We had a decision about he black and flake8 we need to look for length as both the library length varies."
 print(scale)
 
 scale1 = "We had a decision about the h varies. We had a decision about the black and flake8 we need to look for "
